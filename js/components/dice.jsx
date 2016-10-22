605b2ce71801ee8849eb17325113cfcb903c19fe
jbenjaminy <young.benjamin@gmail.com>
2016-10-22 18:59:27 -0500

    updates image var and removes proptypes from dice.jsx

--- js/components/dice.jsx
+++ js/components/dice.jsx
@@ -1,17 +1,11 @@
-import React, { PropTypes } from 'react';
-
-const propTypes = {
-	image: PropTypes.string.isRequired
-};
+import React from 'react';
 
 function Dice(props) {
 	if ((props.image === './dice-roll-one.gif') || (props.image === './dice-roll-two.gif')) {
 		return <img src={props.image}/>
 	}
-	let image = './dice-' + props.image + '.png';
+	let image = `./dice-${props.image}.png`;
 	    return <img src={image}/>
 }
-
-Dice.propTypes = propTypes;
 
 module.exports = Dice;
